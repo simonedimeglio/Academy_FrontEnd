c09a9179ec3e1c1f35ba2e47cdc99852f1a2d564
simonedimeglio <simonedimeglio@icloud.com>
2024-06-07 10:00:39 +0200

    manipolazione DOM

---
+++ JS/manipolazione_DOM.js
@@ -0,0 +1,24 @@
+/*
+SELEZIONARE GLI ELEMENTI -> document
+
+1) document.getElementById('id')
+A che serve -> a selezionare UN SOLO elemento per il suo ID
+Esempio -> const box = document.getElementById('box');
+
+2) document.getElementsByClassName('classe'); 
+A che serve -> a selezionare UNO o PIU' elementi in base alle classi
+Esempio -> const ciaoElements = document.getElementsByClassName('ciao');
+
+3) document.getElementsByTagName('div'); 
+A che serve -> a selezionare UNO o PIU' elementi in base al tipo di tag
+Esempio -> const divs = document.getElementsByTagName('div');
+
+4) document.querySelector('div'); 
+A che serve -> a selezionare il PRIMO elemento che corrisponde alla selezione
+Esempio -> const title = document.querySelector('h1');
+
+5) document.querySelectorAll('a'); 
+A che serve -> a selezionare TUTTI gli elementi che corrispondo alla selezione
+Esempio -> const links = document.querySelectorAll('a');
+*/
+
