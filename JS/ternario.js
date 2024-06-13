d75e991cb9ac4423f86422fc8abbb79fb2337dbb
simonedimeglio <simonedimeglio@icloud.com>
2024-06-13 10:41:53 +0200

    operatore ternario

---
+++ JS/ternario.js
@@ -0,0 +1,23 @@
+// TERNARY 
+
+/**
+ * 
+ * Sintassi: 
+ * 
+ * condizione ? codiceSeVera : codiceSeFalsa;
+ * 
+ */
+
+// Esempio 1 
+
+let numero = 6; 
+let ternario = (numero % 2 === 0) ? "pari" : "dispari";
+console.log(ternario);
+
+
+let età = 12; 
+let accesso = (età < 15) ? "bimbo" : (età < 20) ? "adolescente" : "adulto";
+console.log(accesso);
+
+
+
