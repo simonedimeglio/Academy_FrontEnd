e320911b0cad96c8431810d1e91ff235ad794d27
simonedimeglio <simonedimeglio@icloud.com>
2024-06-14 11:23:20 +0200

    pokeapi

---
+++ JS/pokedex/script.js
@@ -0,0 +1,58 @@
+// Puntatore
+const container = document.getElementById("pokemonContainer");
+
+
+/**
+ * 
+ * Then
+ * 
+ */
+
+const numeroCasuale = Math.floor(Math.random() * 1025) +1 ;
+// fetch(`https://pokeapi.co/api/v2/pokemon/${numeroCasuale}`)
+//     .then(response => response.json())
+//     .then(pokemon => {
+
+//         const h2 = document.createElement("h2");
+//         h2.innerText = `${pokemon.name}`;
+
+//         const img = document.createElement("img"); 
+//         img.src = `${pokemon.sprites.front_default}`;
+
+//         container.append(h2, img);
+//     })
+//     .catch(error => {
+//         console.error(error)
+//     }); 
+        
+
+/**
+ * 
+ * ASYNC AWAIT 
+ * 
+ */
+
+async function ottieniPokemon() {
+    // Faccio la richiesta all'endpoint tramite fetch 
+    const risposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${numeroCasuale}`);
+
+    if (risposta.ok) {
+        // Converto la risposta in json 
+        const pokemon = await risposta.json();
+
+        // Stampo in console i dati del pokemon
+        console.log(pokemon);
+
+        const h2 = document.createElement("h2");
+        h2.innerText = `${pokemon.name}`;
+
+        const img = document.createElement("img"); 
+        img.src = `${pokemon.sprites.front_default}`;
+
+        container.append(h2, img);
+    } else {
+        console.log('ERRORE:', risposta.status)
+    }
+}
+
+ottieniPokemon();
